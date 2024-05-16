a2008eabe064520297625e55a2251e8e44a35e5c
qodirov-a-0110 <qodirovabdumajid03@gmail.com>
2024-05-17 03:21:40 +0500

    dont finished

---
+++ js/index.js
@@ -0,0 +1,43 @@
+// console.log("salom shox");
+// var name ='abdumajid'
+// let sername ="qodirov"
+// const nic ="_abu_"
+
+// console.log(name);
+// console.log(nic);
+// console.log(sername);
+
+// const pr = prompt('yoshinagizni kiritin', 45);
+// let prop = confirm ('ochirmoxchimisiz');
+// console.log(prop);
+// document.write(sername)
+// console.log(pr);
+
+// 1-misol
+// let L = +prompt('uzunligdi sm kiritin');
+// let M = Math.floor(L/100);
+// console.log(M);
+
+// 2-misol
+// let A = +prompt('uzunligdi sm kiritin kg');
+// let t = Math.floor(A/1000);
+// console.log(t);
+
+// 3-misol
+// let bayt = +prompt ('baytni kb otkazish');
+// let kb = Math.floor(bayt/1024);
+// console.log(kb);
+
+// 4-misol
+// let a = +prompt('a kesmadan b dan nechta bor');
+// let b = +prompt('a kesmadan b dan nechta bor');
+// let sum = Math.floor(a/b);
+// console.log(sum);
+
+// 5-misol
+// let a = +prompt('a kesmadan b dan nechta bor');
+// let b = +prompt('a kesmadan b dan nechta bor');
+// let  sum= Math.floor(a/b);
+// let yegindi = (sum*b)
+// console.log(yegindi);
+
